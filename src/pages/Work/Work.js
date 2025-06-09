4c5e0973cfdb7c6af366785ef3f1ec6d46cbc7bf
suhaan <suhaanym1601@gmail.com>
2025-06-09 13:50:14 -0700

    Update Work.js

--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./Work.css";
 import backButton from "../../assets/backButton.png";
 import TechHolder from "../../components/Card/TechHolder";
